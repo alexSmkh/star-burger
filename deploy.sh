bd555dee47c3909a347d3a60993abf507c435875
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-01-28 00:10:01 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ set -e
 
 cd $PROJECT_PATH
 
+source .env
+
 repo_update_result=$(git pull)
 
 if [[ $repo_update_result="Already up to date." ]]
@@ -22,5 +24,23 @@ npm ci
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 
 systemctl restart star_burger.service
+
+last_commit_hash=$(git rev-parse --short HEAD)
+
+# jo is a handy utility for creating json objects. See more details: https://github.com/jpmens/jo
+rollbar_data=$(jo \
+  environment=$PROFILE \
+  revision=$last_commit_hash \
+  rollbar_name=$ROLLBAR_NAME \
+  local_username=$USER \
+  comment="Deployment $last_commit_hash" \
+  status=succeeded \
+)
+
+curl -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" \
+     -H "Content-Type: application/json" \
+     -X POST 'https://api.rollbar.com/api/1/deploy' \
+     -d "$rollbar_data" \
+     -s > /dev/null
 
 echo "Deployment successfully completed"
